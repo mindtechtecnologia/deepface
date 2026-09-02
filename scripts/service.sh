b3bdd149321f938d3fb27e40be5973a8a3edbae5
Mateus Mello <mateus@mindtech.com.br>
2026-09-02 16:54:53 -0300

    fixed docker build

--- scripts/service.sh
+++ scripts/service.sh
@@ -2,7 +2,7 @@
 cd ../deepface/api/src
 
 # run the service with flask - not for production purposes
-# python api.py
+# python app.py
 
 # run the service with gunicorn - for prod purposes
 gunicorn --workers=1 --timeout=3600 --bind=0.0.0.0:5005 "app:create_app()"
